041090f70f7426e37554755f8632ffea1b17c230
riverjoy <tosakarinmylove@gmail.com>
2022-11-25 23:04:34 +0800

    css最小影响原则：类名使用reed-前缀，去掉scoped

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,7 @@
 <template>
-  <button @click="toggle" :class="{checked: value}"><span></span></button>
+  <button @click="toggle" class="reed-switch" :class="{'reed-checked' : value}">
+    <span></span>
+  </button>
 </template>
 
 <script lang="ts">
@@ -18,22 +20,22 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 $h: 22px;
 $h2: $h - 4px;
-button {
+.reed-switch {
   height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
   > span {
     position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
-  &.checked { background: #1890ff;
+  &.reed-checked { background: #1890ff;
     > span { left: calc(100% - #{$h2} - 2px); }
   }
   &:focus { outline: none; }
   &:active {
     > span { width: $h2 + 4px; }
   }
-  &.checked:active {
+  &.reed-checked:active {
     > span { width: $h2 + 4px; margin-left: -4px; }
   }
 }
