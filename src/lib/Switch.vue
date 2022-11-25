1eeab830f4d41c2b867843f9bd7f64ffbf2f5b6b
riverjoy <tosakarinmylove@gmail.com>
2022-11-25 19:57:37 +0800

    Switch css完成

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -9,7 +9,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$h:22px;
+$h2:$h - 4px;
 button{
-
+  width: $h*2;
+  height: $h;
+  background-color: grey;
+  border-radius: $h/2;
+  border: none;
+  position: relative;
+  >span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background:white;
+    border-radius: $h2/2;
+  }
 }
 </style>
