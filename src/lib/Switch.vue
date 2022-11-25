c38c55bc69e9027c6e566bd98665534a2dbbbbf8
riverjoy <tosakarinmylove@gmail.com>
2022-11-25 20:38:26 +0800

    修改样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -19,29 +19,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$h:22px;
-$h2:$h - 4px;
-button{
-  width: $h*2;
-  height: $h;
-  background-color: grey;
-  border-radius: $h/2;
-  border: none;
-  position: relative;
-  >span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:red;
-    border-radius: $h2/2;
-    transition: left 250ms;
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  > span {
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
-  &.selected{
-    >span{
-      left: calc(100% - #{$h2} - 2px);
-    }
+  &.selected { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
+  }
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
+  }
+  &.selected:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
   }
 }
 </style>
