d14ff0769f79d9fbfa541909253d94caef7d9984
riverjoy <tosakarinmylove@gmail.com>
2022-11-26 18:10:24 +0800

    dialog引入展示页

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>Dialog Demo</div>
+  <div>示例一</div>
+  <Button>Dialog</Button>
+  <Dialog></Dialog>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue';
+import Dialog from '../lib/Dialog.vue';
 export default {
-  name:'Dialog Demo'
+  name:'Dialog Demo',
+  components: {Dialog, Button}
 }
 </script>
 
