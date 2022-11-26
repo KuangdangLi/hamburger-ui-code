ac0c75bf3b73068da7fb388cae5a13da160662a8
riverjoy <tosakarinmylove@gmail.com>
2022-11-26 18:03:48 +0800

    dialog初始化

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="reed-dialog-overlay">
+    <div class="reed-dialog-wrapper">
+      <div class="reed-dialog">
+        <header>
+          <span>提示</span>
+          <span>关闭</span>
+        </header>
+        <main>
+          <div>一句话</div>
+        </main>
+        <footer>
+          <Button>OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue';
+export default {
+  name:'Dialog',
+  components: {Button},
+}
+</script>
+
+<style lang="scss">
+
+</style>
