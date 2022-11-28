c38e6ecf69cd3ad620f9d8814a0d1d8b2201d735
riverjoy <tosakarinmylove@gmail.com>
2022-11-28 21:17:45 +0800

    为三个新页面组件创建路由，并在DOC页中添加其对应的链接

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,9 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import Intro from "./views/Intro.vue"
+import Install from "./views/Install.vue"
+import GetStarted from "./views/GetStarted.vue"
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -12,6 +15,9 @@ export const router = createRouter({
   routes:[
     {path:'/',component: Home},
     {path:'/doc',component: Doc,children:[
+        {path:'intro',component: Intro},
+        {path:'install',component: Install},
+        {path:'get-started',component: GetStarted},
         {path:'switch',component: SwitchDemo},
         {path:'dialog',component: DialogDemo},
         {path:'button',component: ButtonDemo},
