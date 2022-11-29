2d9b979bac8df67be1f5a651febce90769f76bef
riverjoy <tosakarinmylove@gmail.com>
2022-11-29 14:46:13 +0800

    抽离用户需要拷贝的代码

---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <Switch v-model:value="bool" :disabled="disable" />
+</template>
+
+<script lang="ts">
+import {ref} from 'vue';
+import Switch from '../lib/Switch.vue';
+export default {
+  name:'Switch2Demo',
+  components: {Switch},
+  setup(){
+    const bool = ref(false)
+    const disable = ref(true)
+    return {bool,disable}
+  }
+}
+</script>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch v-model:value="bool" />
+        <Switch1Demo />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -16,7 +16,7 @@
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch v-model:value="bool2" disabled />
+        <Switch2Demo />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -29,17 +29,13 @@
 </template>
 
 <script lang="ts">
-import Switch from "../lib/Switch.vue"
-import {ref} from 'vue';
 import Button from '../lib/Button.vue';
+import Switch1Demo from './Switch1.demo.vue';
+import Switch2Demo from './Switch2.demo.vue';
+
 export default {
   name:'Switch Demo',
-  setup(){
-    const bool = ref(false)
-    const bool2 = ref(false)
-    return {bool,bool2}
-  },
-  components:{Button, Switch}
+  components:{Switch2Demo, Switch1Demo, Button}
 }
 </script>
 
